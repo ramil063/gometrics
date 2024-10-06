71cc301decfacd0f63af0e255eb39c0687c6ba27
rabdullin <ramil_063@mail.ru>
2024-10-06 23:17:01 +0300

    iter5 переменные окружения

--- cmd/agent/handlers/flags.go
+++ cmd/agent/handlers/flags.go
@@ -1,37 +1,18 @@
 package handlers
 
 import (
-	"errors"
 	"flag"
-	"strconv"
-	"strings"
+	"github.com/caarlos0/env/v6"
 )
 
 var MainURL = "localhost:8080"
 var PollInterval = 2
 var ReportInterval = 10
 
-type NetAddress struct {
-	Host string
-	Port int
-}
-
-func (na NetAddress) String() string {
-	return na.Host + ":" + strconv.Itoa(na.Port)
-}
-
-func (na *NetAddress) Set(s string) error {
-	hp := strings.Split(s, ":")
-	if len(hp) != 2 {
-		return errors.New("need address in a form host:port")
-	}
-	port, err := strconv.Atoi(hp[1])
-	if err != nil {
-		return err
-	}
-	na.Host = hp[0]
-	na.Port = port
-	return nil
+type EnvVars struct {
+	Address        string `env:"ADDRESS"`
+	ReportInterval int    `env:"REPORT_INTERVAL"`
+	PollInterval   int    `env:"POLL_INTERVAL"`
 }
 
 func ParseFlags() {
@@ -39,4 +20,17 @@ func ParseFlags() {
 	flag.IntVar(&ReportInterval, "r", 10, "report interval in seconds")
 	flag.IntVar(&PollInterval, "p", 2, "poll interval in seconds")
 	flag.Parse()
+
+	var ev EnvVars
+	_ = env.Parse(&ev)
+
+	if ev.Address != "" {
+		MainURL = ev.Address
+	}
+	if ev.ReportInterval != 0 {
+		ReportInterval = ev.ReportInterval
+	}
+	if ev.PollInterval != 0 {
+		PollInterval = ev.PollInterval
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ramil063/gometrics
 go 1.22.7
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/stretchr/testify v1.9.0
 )
