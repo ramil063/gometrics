317e23e0273d18958115f4e2e17c63cbc6f7117e
rabdullin <ramil_063@mail.ru>
2025-08-12 03:48:29 +0300

    iter25 статик тест

--- cmd/server/handlers/middlewares/middlewares_test.go
+++ cmd/server/handlers/middlewares/middlewares_test.go
@@ -634,8 +634,7 @@ func TestDecryptMiddleware(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var decryptor crypto.Decryptor
-			decryptor = tt.decryptor
+			decryptor := tt.decryptor
 
 			body := bytes.NewReader(tt.requestBody)
 
